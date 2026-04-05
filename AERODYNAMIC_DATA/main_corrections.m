ebd8815f75116629b1aac69ea73ff82f89331858
Avonian <menakul2@gmail.com>
2026-04-05 14:00:22 +0200

    refactoring + extra factoring + hallucinations

--- AERODYNAMIC_DATA/main_corrections.m
+++ AERODYNAMIC_DATA/main_corrections.m
@@ -56,39 +56,46 @@ load("TailOff_BAL.mat")
 clear diskPath fn0 fn_BAL idxB
 
 %% separating data
-block_names = fieldnames(BAL.windOn);
+% block_names = fieldnames(BAL.windOn);
+% 
+% propOff_uncorrected_disjoint = struct();
+% propOn_uncorrected_disjoint = struct();
+% for i = 1:numel(block_names) 
+%     block = block_names{i};
+%     block_length = length(BAL.windOn.(block).AoA);
+%     % Rudder deflection variables
+%     if contains(block, "0")
+%         if contains(block, "m10")
+%             BAL.windOn.(block).dR = -10 * ones(block_length,1);
+%         elseif contains(block, "p10")
+%             BAL.windOn.(block).dR = 10 * ones(block_length,1);
+%         else
+%             BAL.windOn.(block).dR = zeros(block_length,1);
+%         end
+%     elseif contains(block, "5")
+%         BAL.windOn.(block).dR = 5 * ones(block_length,1);
+%     end
+% 
+%     % combining blocks
+%     if contains(block, "block1") % block 1 is propOff measurements
+%         propOff_uncorrected_disjoint.(block) = BAL.windOn.(block);
+%     else
+%         propOn_uncorrected_disjoint.(block) = BAL.windOn.(block);
+%     end
+% end
+% clear block block_names block_length
 
-propOff_uncorrected_disjoint = struct();
-propOn_uncorrected_disjoint = struct();
-for i = 1:numel(block_names) 
-    block = block_names{i};
-    block_length = length(BAL.windOn.(block).AoA);
-    % Rudder deflection variables
-    if contains(block, "0")
-        if contains(block, "m10")
-            BAL.windOn.(block).dR = -10 * ones(block_length,1);
-        elseif contains(block, "p10")
-            BAL.windOn.(block).dR = 10 * ones(block_length,1);
-        else
-            BAL.windOn.(block).dR = zeros(block_length,1);
-        end
-    elseif contains(block, "5")
-        BAL.windOn.(block).dR = 5 * ones(block_length,1);
-    end
-
-    % combining blocks
-    if contains(block, "block1") % block 1 is propOff measurements
-        propOff_uncorrected_disjoint.(block) = BAL.windOn.(block);
-    else
-        propOn_uncorrected_disjoint.(block) = BAL.windOn.(block);
-    end
-end
-clear block block_names block_length
+[propOn_uncorrected_disjoint, propOff_uncorrected_disjoint] = rudder_prop_corrector(BAL.windOn);
 %%
 propOff_uncorrected = blocks_superglue(propOff_uncorrected_disjoint);
 propOn_uncorrected = blocks_superglue(propOn_uncorrected_disjoint);
 clear propOff_uncorrected_disjoint propOn_uncorrected_disjoint
 % freeing up memory ^
+
+propOff_uncorrected.Vol_model = 0.022 * ones(length(propOff_uncorrected.AoA));
+propOn_uncorrected.Vol_model = 0.022 * ones(length(propOn_uncorrected.AoA));
+propOff_uncorrected.Ksb = 0.96 * ones(length(propOff_uncorrected.AoA));
+propOn_uncorrected.Vol_model = 0.96 * ones(length(propOn_uncorrected.AoA));
 %% User inputs for corrections
 
 At = 2.07;      % tunnel test-section area [m^2]
@@ -97,25 +104,25 @@ alpha_up = 0.0; % tunnel upflow correction [deg]
 %% not including struts
 %% 0.0259787
 % volume of the Model
-Vol_model = struct();
-Vol_model.rudder_0_block1     = 0.022;
-Vol_model.rudder_0_block3     = 0.022;
-Vol_model.rudder_0_block4     = 0.022;
-Vol_model.rudder_m10_block6_7 = 0.022;
-Vol_model.rudder_p5_block7b   = 0.022;
-Vol_model.rudder_p10_block1   = 0.022;
-Vol_model.rudder_p10_block5   = 0.022;
+% Vol_model = struct();
+% Vol_model.rudder_0_block1     = 0.022;
+% Vol_model.rudder_0_block3     = 0.022;
+% Vol_model.rudder_0_block4     = 0.022;
+% Vol_model.rudder_m10_block6_7 = 0.022;
+% Vol_model.rudder_p5_block7b   = 0.022;
+% Vol_model.rudder_p10_block1   = 0.022;
+% Vol_model.rudder_p10_block5   = 0.022;
 
 
 %% what should this be ?
-Ksb = struct();
-Ksb.rudder_0_block1     = 0.90;
-Ksb.rudder_0_block3     = 0.90;
-Ksb.rudder_0_block4     = 0.90;
-Ksb.rudder_m10_block6_7 = 0.90;
-Ksb.rudder_p5_block7b   = 0.90;
-Ksb.rudder_p10_block1   = 0.90;
-Ksb.rudder_p10_block5   = 0.90;
+% Ksb = struct();
+% Ksb.rudder_0_block1     = 0.90;
+% Ksb.rudder_0_block3     = 0.90;
+% Ksb.rudder_0_block4     = 0.90;
+% Ksb.rudder_m10_block6_7 = 0.90;
+% Ksb.rudder_p5_block7b   = 0.90;
+% Ksb.rudder_p10_block1   = 0.90;
+% Ksb.rudder_p10_block5   = 0.90;
 
 delta = struct();
 %% what should be the value of delta??? its used in corrections later 
@@ -139,47 +146,107 @@ dCm_dCL.rudder_p10_block5   = 0.0;
 
 %% Apply corrections
 
-cfgNames = fieldnames(BAL.windOn);
-nCfg = numel(cfgNames);
+% cfgNames = fieldnames(BAL.windOn);
+% nCfg = numel(cfgNames);
 
-BALc = BAL; % corrected copy
+% BALc = BAL; % corrected copy
 
-% -------------------------------------------------------------
-% Store all data in separate lists
-% -------------------------------------------------------------
+% % -------------------------------------------------------------
+% % Store all data in separate lists
+% % -------------------------------------------------------------
 
-cfg_list = cfgNames;
+% cfg_list = cfgNames;
 
-% raw data
-AoA_list     = cell(nCfg,1);
-AoS_list     = cell(nCfg,1);
-q_list       = cell(nCfg,1);
-CL_list      = cell(nCfg,1);
-CD_list      = cell(nCfg,1);
-CY_list      = cell(nCfg,1);
-CMroll_list  = cell(nCfg,1);
-CMpitch_list = cell(nCfg,1);
-CMyaw_list   = cell(nCfg,1);
+% % raw data
+% AoA_list     = cell(nCfg,1);
+% AoS_list     = cell(nCfg,1);
+% q_list       = cell(nCfg,1);
+% CL_list      = cell(nCfg,1);
+% CD_list      = cell(nCfg,1);
+% CY_list      = cell(nCfg,1);
+% CMroll_list  = cell(nCfg,1);
+% CMpitch_list = cell(nCfg,1);
+% CMyaw_list   = cell(nCfg,1);
 
-% corrected data
-q_corr_list       = cell(nCfg,1);
-AoA_corr_list     = cell(nCfg,1);
-CL_corr_list      = cell(nCfg,1);
-CD_corr_list      = cell(nCfg,1);
-CY_corr_list      = cell(nCfg,1);
-CMroll_corr_list  = cell(nCfg,1);
-CMpitch_corr_list = cell(nCfg,1);
-CMyaw_corr_list   = cell(nCfg,1);
+% % corrected data
+% q_corr_list       = cell(nCfg,1);
+% AoA_corr_list     = cell(nCfg,1);
+% CL_corr_list      = cell(nCfg,1);
+% CD_corr_list      = cell(nCfg,1);
+% CY_corr_list      = cell(nCfg,1);
+% CMroll_corr_list  = cell(nCfg,1);
+% CMpitch_corr_list = cell(nCfg,1);
+% CMyaw_corr_list   = cell(nCfg,1);
 
-% correction terms
-eps_sb_list  = cell(nCfg,1);
-eps_wb_list  = cell(nCfg,1);
-eps_tot_list = cell(nCfg,1);
+% % correction terms
+% eps_sb_list  = cell(nCfg,1);
+% eps_wb_list  = cell(nCfg,1);
+% eps_tot_list = cell(nCfg,1);
 
 
-CLw_data = load('CLw_lookup.mat');
+% CLw_data = load('CLw_lookup.mat');
+% Load the .mat file
+data = load('CLw_lookup.mat');
+CLw_data = struct();
+
+% === Block 1 (rudder_0_block1) ===
+CLw_data.rudder_0_block.AoA = data.rudder_0_block1_AoA;
+CLw_data.rudder_0_block.AoS = data.rudder_0_block1_AoS;
+CLw_data.rudder_0_block.V   = data.rudder_0_block1_V;
+CLw_data.rudder_0_block.Cl  = data.rudder_0_block1;
+
+% === Block 3 (rudder_0_block3) ===
+CLw_data.rudder_0_block3.AoA = data.rudder_0_block3_AoA;
+CLw_data.rudder_0_block3.AoS = data.rudder_0_block3_AoS;
+CLw_data.rudder_0_block3.V   = data.rudder_0_block3_V;
+CLw_data.rudder_0_block3.Cl  = data.rudder_0_block3;
+
+% === Block 4 (rudder_0_block4) ===
+CLw_data.rudder_0_block4.AoA = data.rudder_0_block4_AoA;
+CLw_data.rudder_0_block4.AoS = data.rudder_0_block4_AoS;
+CLw_data.rudder_0_block4.V   = data.rudder_0_block4_V;
+CLw_data.rudder_0_block4.Cl  = data.rudder_0_block4;
+
+% === Block m10 (rudder_m10_block6_7) ===
+CLw_data.block_m10.AoA = data.rudder_m10_block6_7_AoA;
+CLw_data.block_m10.AoS = data.rudder_m10_block6_7_AoS;
+CLw_data.block_m10.V   = data.rudder_m10_block6_7_V;
+CLw_data.block_m10.Cl  = data.rudder_m10_block6_7;
+
+% === Block p5 (rudder_p5_block7b) ===
+CLw_data.block_p5.AoA = data.rudder_p5_block7b_AoA;
+CLw_data.block_p5.AoS = data.rudder_p5_block7b_AoS;
+CLw_data.block_p5.V   = data.rudder_p5_block7b_V;
+CLw_data.block_p5.Cl  = data.rudder_p5_block7b;
+
+% === Block p10_1 (rudder_p10_block1) ===
+CLw_data.block_p10_1.AoA = data.rudder_p10_block1_AoA;
+CLw_data.block_p10_1.AoS = data.rudder_p10_block1_AoS;
+CLw_data.block_p10_1.V   = data.rudder_p10_block1_V;
+CLw_data.block_p10_1.Cl  = data.rudder_p10_block1;
+
+% === Block p10_5 (rudder_p10_block5) ===
+CLw_data.block_p10_5.AoA = data.rudder_p10_block5_AoA;
+CLw_data.block_p10_5.AoS = data.rudder_p10_block5_AoS;
+CLw_data.block_p10_5.V   = data.rudder_p10_block5_V;
+CLw_data.block_p10_5.Cl  = data.rudder_p10_block5;
+
+clear data
+
 %% fieldnames(CLw_data)
 %% cfgNames
+
+% Ensure propOn and propOff have same fieldnames
+propOn_fields = fieldnames(propOn_uncorrected);
+proOff_fields = fieldnames(propOff_uncorrected);
+if propOn_fields ~= proOff_fields
+    missing_fields = [setdiff(propOn_fields, proOff_fields), setdiff(propOff_fields, propOn_fields)];
+    error("Data sets do not have the same fields: %s", join(string(missing_fields)), "\n")
+end
+
+cfgNames = propOn_fields;
+nCfg = numel(cfgNames);
+clear propOn_fields proOff_fields
 
 for i = 1:nCfg
     nm = cfgNames{i};
