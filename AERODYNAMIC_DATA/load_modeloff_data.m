2ff682fc61a095fa6714f9eb2b248310c70e621a
Ashwin2915 <152791942+Ashwin2915@users.noreply.github.com>
2026-04-06 15:12:33 +0200

    Update load_modeloff_data.m
    changed Cy to CY

--- AERODYNAMIC_DATA/load_modeloff_data.m
+++ AERODYNAMIC_DATA/load_modeloff_data.m
@@ -4,8 +4,8 @@ function modelOff = load_modeloff_data(filename)
 T1 = readtable(filename, 'Sheet', 1, 'Range', 'A12:H31', 'VariableNamingRule', 'preserve');
 T2 = readtable(filename, 'Sheet', 1, 'Range', 'J12:Q32', 'VariableNamingRule', 'preserve');
 
-T1.Properties.VariableNames = {'AoA','AoS','CD','Cy','CL','CMroll','CMpitch','CMyaw'};
-T2.Properties.VariableNames = {'AoA','AoS','CD','Cy','CL','CMroll','CMpitch','CMyaw'};
+T1.Properties.VariableNames = {'AoA','AoS','CD','CY','CL','CMroll','CMpitch','CMyaw'};
+T2.Properties.VariableNames = {'AoA','AoS','CD','CY','CL','CMroll','CMpitch','CMyaw'};
 
 % remove empty rows
 T1 = rmmissing(T1, 'DataVariables', {'AoA','AoS'});
