ef19ee26ef5d4afb2e571ae6e46471ef525f95c0
Avonian <menakul2@gmail.com>
2026-04-06 14:38:26 +0200

    derives chord from data now

--- AERODYNAMIC_DATA/blockage_corrections.m
+++ AERODYNAMIC_DATA/blockage_corrections.m
@@ -9,6 +9,7 @@ function data_corrected = blockage_corrections(data, tailOff, At, TCStar)
     CD       = data.CD;
     eps_sb   = Ksb* V_model/ (At^(3/2));
     alpha_up = 0;
+    c        = data.c;
     
     % eps_wb = 0.25 * data.CD;   % placeholder, replace if needed
     eps_wb  = (S_wing * CD) / (4 * At); % attached flow, separated eps = 0
@@ -18,11 +19,11 @@ function data_corrected = blockage_corrections(data, tailOff, At, TCStar)
     
     % ---------- 2) Recompute coefficients ----------
     data_corrected.q       = data.q       .* (1 + eps_tot).^2;
-    data_corrected.CL      = data.CL      ./ (1 + eps_tot).^2;
-    data_corrected.CD      = data.CD      ./ (1 + eps_tot).^2;
-    data_corrected.CY      = data.CY      ./ (1 + eps_tot).^2;
-    data_corrected.CMroll  = data.CMroll  ./ (1 + eps_tot).^2;
-    data_corrected.CMpitch = data.CMpitch ./ (1 + eps_tot).^2;
+    data_corrected.CL      = data.CL      ./ (1 + eps_tot).^2; % corrected lift coefficient
+    data_corrected.CD      = data.CD      ./ (1 + eps_tot).^2; 
+    data_corrected.CY      = data.CY      ./ (1 + eps_tot).^2; % corrected side-force coefficient
+    data_corrected.CMroll  = data.CMroll  ./ (1 + eps_tot).^2; % corrected rolling-moment coefficient
+    data_corrected.CMpitch = data.CMpitch ./ (1 + eps_tot).^2; % corrected pitching-moment coefficient
     data_corrected.CMyaw   = data.CMyaw   ./ (1 + eps_tot).^2;
 
     dalpha_w           = data.delta .* CLw .* 57.3;
@@ -34,6 +35,11 @@ function data_corrected = blockage_corrections(data, tailOff, At, TCStar)
     dCD_w  = data.delta .* CLw.^2;
     data_corrected.CD = CDu + dCD_up + dCD_w;
 
-    % data_corrected.CMpitch = data_corrected.CMpitch - dCm_dCL .* data_corrected.CMroll; % What is dCM_dCL value
-    
+    % data_corrected.CMpitch = data_corrected.CMpitch - dCm_dCL .* data_corrected.CMroll;  % corrected pitching-moment coefficient
+    % What is dCM_dCL value ^
+
+
+    %% Downwash Correction
+    % del_alphaT = delta S_wing/c
+
 end
