fb32e136e9efd69c552f04bba387f445af5f303c
Avonian <menakul2@gmail.com>
2026-04-07 11:26:26 +0200

    removed uneeded comments

--- AERODYNAMIC_DATA/blockage_corrections.m
+++ AERODYNAMIC_DATA/blockage_corrections.m
@@ -43,39 +43,3 @@ function data_corrected = blockage_corrections(data, tailOff, At, TCStar)
     % del_alphaT = delta S_wing/c
 
 end
-
-% data = propOn_uncorrected;
-% tailOff = tailOff_
-% 
-% CLw      = tailOff.CL;
-% Ksb      = data.Ksb;
-% V_model  = data.Vol_model;
-% S_wing   = data.S;
-% CD       = data.CD;
-% eps_sb   = Ksb* V_model/ (At^(3/2));
-% alpha_up = 0;
-% c        = data.c;
-% 
-% % eps_wb = 0.25 * data.CD;   % placeholder, replace if needed
-% eps_wb  = (S_wing * CD) / (4 * At); % attached flow, separated eps = 0
-% eps_ss  = slipstream_interference(TCStar);
-% eps_tot = eps_sb + eps_wb + eps_ss;
-% 
-% 
-% % ---------- 2) Recompute coefficients ----------
-% data_corrected.q       = data.q       .* (1 + eps_tot).^2;
-% data_corrected.CL      = data.CL      ./ (1 + eps_tot).^2; % corrected lift coefficient
-% data_corrected.CD      = data.CD      ./ (1 + eps_tot).^2;
-% data_corrected.CY      = data.CY      ./ (1 + eps_tot).^2; % corrected side-force coefficient
-% data_corrected.CMroll  = data.CMroll  ./ (1 + eps_tot).^2; % corrected rolling-moment coefficient
-% data_corrected.CMpitch = data.CMpitch ./ (1 + eps_tot).^2; % corrected pitching-moment coefficient
-% data_corrected.CMyaw   = data.CMyaw   ./ (1 + eps_tot).^2;
-% 
-% dalpha_w           = data.delta .* CLw .* 57.3;
-% dalpha_up_rad      = deg2rad(alpha_up);
-% data_corrected.AoA = data.AoA + alpha_up + dalpha_w;
-% 
-% 
-% dCD_up = CLw .* dalpha_up_rad;
-% dCD_w  = data.delta .* CLw.^2;
-% data_corrected.CD = CDu + dCD_up + dCD_w;
