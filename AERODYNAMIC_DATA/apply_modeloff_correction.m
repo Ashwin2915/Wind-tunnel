b03f0dd1725863bc3cd3ec5b70b4395e349713f5
Ashwin2915 <152791942+Ashwin2915@users.noreply.github.com>
2026-04-07 14:03:06 +0200

    changed to hopefully fix any row column vector mismatch

--- AERODYNAMIC_DATA/apply_modeloff_correction.m
+++ AERODYNAMIC_DATA/apply_modeloff_correction.m
@@ -1,4 +1,3 @@
-
 function dataOut = apply_modeloff_correction(dataIn, modelOff)
 % Apply model-off correction using:
 % Cx(a,b) = Cx(a,0) + Cx(0,b) - Cx(0,0)
@@ -13,36 +12,84 @@ coeffNames = {'CD','CY','CL','CMroll','CMpitch','CMyaw'};
 for k = 1:numel(coeffNames)
     nm = coeffNames{k};
 
-    % force interpolation tables to columns too
-    alphaA = modelOff.alpha0.AoA(:);
-    alphaC = modelOff.alpha0.(nm)(:);
+    xA = modelOff.alpha0.AoA(:);
+    yA = modelOff.alpha0.(nm)(:);
 
-    betaB  = modelOff.beta0.AoS(:);
-    betaC  = modelOff.beta0.(nm)(:);
+    xB = modelOff.beta0.AoS(:);
+    yB = modelOff.beta0.(nm)(:);
 
-    % interpolated model-off terms
-    C_a0 = interp1(alphaA, alphaC, AoA, 'linear', 'extrap');
-    C_0b = interp1(betaB,  betaC,  AoS, 'linear', 'extrap');
+    Cin = dataIn.(nm)(:);
 
-    % make sure both are columns
+    C_a0 = interp1(xA, yA, AoA, 'linear', 'extrap');
+    C_0b = interp1(xB, yB, AoS, 'linear', 'extrap');
+
     C_a0 = C_a0(:);
     C_0b = C_0b(:);
 
-    % value at alpha = 0, beta = 0
-    C_00_a = interp1(alphaA, alphaC, 0, 'linear', 'extrap');
-    C_00_b = interp1(betaB,  betaC,  0, 'linear', 'extrap');
+    C_00_a = interp1(xA, yA, 0, 'linear', 'extrap');
+    C_00_b = interp1(xB, yB, 0, 'linear', 'extrap');
     C_00 = 0.5 * (C_00_a + C_00_b);
 
-    % total model-off correction
     C_modeloff = C_a0 + C_0b - C_00;
 
-    % subtract from measured coefficient
-    dataOut.(nm) = dataIn.(nm)(:) - C_modeloff;
-
-    % optional: store correction itself
+    dataOut.(nm) = Cin - C_modeloff;
     dataOut.([nm '_modeloff']) = C_modeloff;
+
+    fprintf('\n%s\n', nm)
+    disp(size(Cin))
+    disp(size(C_a0))
+    disp(size(C_0b))
+    disp(size(C_modeloff))
+    disp(size(dataOut.(nm)))
 end
 end
+
+% FIX 1
+
+function dataOut = apply_modeloff_correction(dataIn, modelOff)
+% % Apply model-off correction using:
+% % Cx(a,b) = Cx(a,0) + Cx(0,b) - Cx(0,0)
+
+% dataOut = dataIn;
+
+% AoA = dataIn.AoA(:);
+% AoS = dataIn.AoS(:);
+
+% coeffNames = {'CD','CY','CL','CMroll','CMpitch','CMyaw'};
+
+% for k = 1:numel(coeffNames)
+%     nm = coeffNames{k};
+
+%     % force interpolation tables to columns too
+%     alphaA = modelOff.alpha0.AoA(:);
+%     alphaC = modelOff.alpha0.(nm)(:);
+
+%     betaB  = modelOff.beta0.AoS(:);
+%     betaC  = modelOff.beta0.(nm)(:);
+
+%     % interpolated model-off terms
+%     C_a0 = interp1(alphaA, alphaC, AoA, 'linear', 'extrap');
+%     C_0b = interp1(betaB,  betaC,  AoS, 'linear', 'extrap');
+
+%     % make sure both are columns
+%     C_a0 = C_a0(:);
+%     C_0b = C_0b(:);
+
+%     % value at alpha = 0, beta = 0
+%     C_00_a = interp1(alphaA, alphaC, 0, 'linear', 'extrap');
+%     C_00_b = interp1(betaB,  betaC,  0, 'linear', 'extrap');
+%     C_00 = 0.5 * (C_00_a + C_00_b);
+
+%     % total model-off correction
+%     C_modeloff = C_a0 + C_0b - C_00;
+
+%     % subtract from measured coefficient
+%     dataOut.(nm) = dataIn.(nm)(:) - C_modeloff;
+
+%     % optional: store correction itself
+%     dataOut.([nm '_modeloff']) = C_modeloff;
+% end
+% end
 
 % function dataOut = apply_modeloff_correction(dataIn, modelOff)
 % % Apply model-off correction using:
